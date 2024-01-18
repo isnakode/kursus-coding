3ea2734652b86fa208382e9a8689a34dc2b2197a
isnakode <isnainisasaki@gmail.com>
2024-01-18 14:52:47 +0800

    Updates

---
+++ _app/immutable/chunks/NoScrolbar.JtwBeAV6.js
@@ -0,0 +1,6 @@
+import{s as l,n}from"./scheduler.tpT7kb9E.js";import{S as d,i as c,e as i,C as h,c as m,m as p,d as s,h as u}from"./index.QIhuJUfD.js";function _(r){let e,a=`body::-webkit-scrollbar {\r
+      display: none;\r
+    }\r
+    body {\r
+      scrollbar-width: none;\r
+    }`;return{c(){e=i("style"),e.textContent=a},l(o){const t=h("svelte-fes87a",document.head);e=m(t,"STYLE",{"data-svelte-h":!0}),p(e)!=="svelte-1n0d5jn"&&(e.textContent=a),t.forEach(s)},m(o,t){u(document.head,e)},p:n,i:n,o:n,d(o){s(e)}}}class x extends d{constructor(e){super(),c(this,e,null,_,l,{})}}export{x as N};
